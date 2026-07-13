9f58b14a3fd0c035ecb07bb719de068eb7c4728f
Dikshya-Thapa <dikshya428@gmail.com>
2026-07-13 11:16:37 +0545

    Add backend validation for notes

--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import * as noteController from '../controllers/noteController.js'
+import {
+  noteRules,
+  noteUpdateRules,
+  validateNote,
+} from '../validators/noteValidator.js'
 
 const router = express.Router()
 
 router.get('/', noteController.getNotes)
 
-router.post('/', noteController.addNote)
+router.post('/', noteRules, validateNote, noteController.addNote)
 
-router.put('/:id', noteController.editNote)
+router.put('/:id', noteUpdateRules, validateNote, noteController.editNote)
 
 router.delete('/:id', noteController.removeNote)
 
