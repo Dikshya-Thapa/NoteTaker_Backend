80ebdce11b2abc795923bdd4d50c1c2debf43a5c
Dikshya-Thapa <dikshya428@gmail.com>
2026-07-13 10:42:20 +0545

    Add backend env config and CORS setup

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
 import noteRoutes from './src/routes/noteRoutes.js'
 
 const app = express()
-const PORT = 3001
+dotenv.config()
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/notes', noteRoutes)
 
