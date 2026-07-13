b2c18c3254355b9527a11df0bb093000bba4de3d
Dikshya-Thapa <dikshya428@gmail.com>
2026-07-13 12:10:28 +0545

    Connect backend to MongoDB database

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import noteRoutes from './src/routes/noteRoutes.js'
+import dbConnection from './src/config/db.js'
 
 const app = express()
 dotenv.config()
@@ -11,6 +12,8 @@ app.use(express.json())
 app.use(cors())
 
 app.use('/notes', noteRoutes)
+
+await dbConnection()
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
