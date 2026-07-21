9c9ca6faa4cea1241dffc2ee50b15990a28e81b2
Dikshya-Thapa <dikshya428@gmail.com>
2026-07-21 12:04:29 +0545

    Add authentication routes

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import noteRoutes from './src/routes/noteRoutes.js'
+import authRoutes from './src/routes/authRoutes.js'
 import dbConnection from './src/config/db.js'
 
-const app = express()
 dotenv.config()
-const PORT = process.env.PORT || 3000
 
-app.use(express.json())
+const app = express()
+const PORT = process.env.PORT || 3001
+
 app.use(cors())
+app.use(express.json())
 
 app.use('/notes', noteRoutes)
+app.use('/auth', authRoutes)
 
 await dbConnection()
 
